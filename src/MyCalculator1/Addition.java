ed64d47af945ef7a1ca66c613a5c3541a803b2e6
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-08-17 09:44:01 +0530

    updated one

--- src/MyCalculator1/Addition.java
+++ src/MyCalculator1/Addition.java
@@ -2,6 +2,7 @@ package MyCalculator1;
 
 public class Addition {
 	void add() {
+		System.out.println("addition");
 		
 	}
 }
--- src/MyCalculator1/Substraction.java
+++ src/MyCalculator1/Substraction.java
@@ -2,6 +2,6 @@ package MyCalculator1;
 
 public class Substraction {
 	void subract() {
-		
+		System.out.println("substraction");
 	}
 }
